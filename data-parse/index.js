5dffc91a5b5d67300eb6eac600f8dc0bb0127c8e
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2022-12-12 19:36:14 -0300

    teste query

--- data-parse/index.js
+++ data-parse/index.js
@@ -7,13 +7,12 @@ const fs = require('fs');
 const json = require('./blhs');
 
 (async () => {
-    const asyncRes = await Promise.all(json.slice(0, 1).map((async (e) => {
+    const asyncRes = await Promise.all(json.map((async (e) => {
         try {
             const info = await CepCoords.getByCep(e.cep.replace("-", ""));
             e.lat = info.lat;
             e.lon = info.lon;
         } catch (err) {
-            console.log(err);
         }
         return e;
     })));
--- queries.md
+++ queries.md
@@ -2,10 +2,10 @@ MATCH (n) detach DELETE n
 
 #incluido BLHS
 
-WITH "https://github.com/wbfreitas/fiap-datalake/tree/main/csv-data/" AS base
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
 WITH base + "blhs.csv" AS uri
 LOAD CSV WITH HEADERS FROM uri AS row
-MERGE (:BLHS {id: row.id});
-MERGE (:BLHS {nome: row.nome, endereco: row.endereco,cep: row.cep, uf: row.uf, id: row.id});
+MERGE (:BLHS {nome: row.nome});
+
 
 
