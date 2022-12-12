f4208caa21624df183eb457cea132361848290a9
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2022-12-12 15:00:20 -0300

    feat: tranformando dado em csv

--- data-parse/index.js
+++ data-parse/index.js
@@ -1,17 +1,38 @@
 const CepCoords = require("coordenadas-do-cep");
+const { Parser } = require('json2csv');
+const fs = require('fs');
+
+
+
 const json = require('./blhs');
 
 (async () => {
-    const asyncRes = await Promise.all(json.slice(0, 1).map((async (e) => {
+    const asyncRes = await Promise.all(json.map((async (e) => {
         try {
             const info = await CepCoords.getByCep(e.cep.replace("-", ""));
             e.lat = info.lat;
             e.lon = info.lon;
         } catch (err) {
-        
         }
         return e;
     })));
 
-    console.log(asyncRes);
+    parseToCSV(asyncRes);
 })();
+
+function parseToCSV(data) {
+    try {
+        const parser = new Parser();
+        const csv = parser.parse(data);
+        saveCSV(csv);
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+function saveCSV(csv) {
+    fs.writeFile('../csv-data/blhs.csv', csv, function (err) {
+        if (err) return console.log(err);
+        console.log('CSV salvo!');
+      });
+}
