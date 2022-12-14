e67f37844eeaca40f489d32e763facb8de783326
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2022-12-13 23:00:18 -0300

    teste

--- data-parse/index.js
+++ data-parse/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const BLHs = require('./blhs');
 const nomes = require('./nomes');
 const estados = require('./estados');
-const multiplicador = 3;
+const multiplicador = 1;
 
 
 let doadoras = nomes.slice(0, (nomes.length -1));
