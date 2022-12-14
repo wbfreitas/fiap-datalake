bc4eb8a8953e28cd02082e8b3276ae91ff6413f2
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2022-12-13 21:32:46 -0300

    mudado estrategia

--- data-parse/index.js
+++ data-parse/index.js
@@ -1,43 +1,60 @@
-const CepCoords = require("coordenadas-do-cep");
 const { Parser } = require('json2csv');
 const fs = require('fs');
 
+const BLHs = require('./blhs');
+const nomes = require('./nomes');
+const estados = require('./estados');
 
 
-const json = require('./blhs');
+const doadoras = nomes.slice(0, (nomes.length -1));
+const donatarias = nomes.slice(((nomes.length - 1) /2 ) + 1, nomes.length -1);
+const BLHsNomeID = BLHs.map((b, i) => ({id: i, nome: b.nome}));
+const doadorasBLHs = linkPessoasBLH(doadoras);
+const donatariasBLHs = linkPessoasBLH(donatarias);
 
-(async () => {
-    const asyncRes = await Promise.all(json.map((async (e) => {
-        try {
-            const info = await CepCoords.getByCep(e.cep.replace("-", ""));
-            e.lat = info.lat;
-            e.lon = info.lon;
-        } catch (err) {
-        }
-        return e;
-    })));
+function linkPessoasBLH(pessoas) {
+    return pessoas.map(p => {
+        const blhAleatorio = Math.floor(Math.random() * (BLHs.length - 2));
+        const b = BLHs[blhAleatorio];
+        b.id = blhAleatorio;
 
-    asyncRes.map((s, i) => {
-    s.id = i+1;
-    return s;
-   }); 
+        return {nome: p, blh: b};
+    }); 
+}
 
-    parseToCSV(asyncRes);
-})();
-
-function parseToCSV(data) {
+function parseToCSV(data, fileName) {
     try {
         const parser = new Parser();
         const csv = parser.parse(data);
-        saveCSV(csv);
+        return csv;
       } catch (err) {
-        console.error(err);
+        return "ERROR";
       }
 }
 
-function saveCSV(csv) {
-    fs.writeFile('../csv-data/blhs.csv', csv, function (err) {
+function saveCSV(file, fileName) {
+    const csv = parseToCSV(file);
+    fs.writeFile('../csv-data/' + fileName, csv, function (err) {
         if (err) return console.log(err);
         console.log('CSV salvo!');
-      });
+    });
 }
+
+saveCSV(doadoras.map(d => ({nome: d})), 'doadoras.csv');
+saveCSV(donatarias.map(d => ({nome: d})), 'donatarias.csv');
+saveCSV(BLHsNomeID, 'blhs.csv');
+saveCSV(estados, 'estados.csv');
+saveCSV(doadorasBLHs.map(d => ({blh: d.blh.id, nome: d.nome})), 'doadoras-blhs.csv');
+saveCSV(donatariasBLHs.map(d => ({blh: d.blh.id, nome: d.nome})), 'donatarias-blhs.csv');
+
+saveCSV(BLHs.map((b, i) => ({
+    id: i, uf: b.uf
+})), 'blhs-estados.csv');
+
+saveCSV(doadorasBLHs.map((d) => ({
+    name: d.nome, uf: d.blh.uf
+})), 'doadoras-estados.csv');
+
+saveCSV(donatariasBLHs.map((d) => ({
+    name: d.nome, uf: d.blh.uf
+})), 'donatarias-estados.csv');
--- queries.md
+++ queries.md
@@ -1,11 +1,66 @@
+#limpando dados
 MATCH (n) detach DELETE n
 
-#incluido BLHS
-
+#incluido BLHs
 WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
 WITH base + "blhs.csv" AS uri
 LOAD CSV WITH HEADERS FROM uri AS row
-MERGE (:BLHS {nome: row.nome});
+MERGE (:BLHs {nome: row.nome, id: row.id });
 
+#incluido doadoras
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "doadoras.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MERGE (:DOADORAS {nome: row.nome });
 
+#incluido donatarias
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "donatarias.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MERGE (:DONATARIAS {nome: row.nome });
 
+#incluido Estados 
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "estados.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MERGE (:UFs {nome: row.uf });
+
+#Ligando doadoras e BLHs
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "doadoras-blhs.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(destino:BLHs {id: row.blh})
+MATCH(doadora:DOADORAS {nome: row.nome})
+MERGE (doadora)-[:DOA]->(destino);
+
+#Ligando donatarias e BLHs
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "doadoras.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(origem:BLHs {id: row.blh})
+MATCH(destino:DONATARIAS {nome: row.nome})
+MERGE (origem)-[:DOA]->(destino);
+
+#Estado Doadora
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "doadoras-estados.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(estado:UFs {name: row.uf})
+MATCH(doadora:DOADORAS {nome: row.nome})
+MERGE (doadora)-[:RESIDE]->(estado);
+
+#Estado Donatarias 
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "donatarias-estados.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(estado:UFs {name: row.uf})
+MATCH(doadora:DONATARIAS {nome: row.nome})
+MERGE (doadora)-[:RESIDE]->(estado);
+
+#Estado BLHs 
+WITH "https://raw.githubusercontent.com/wbfreitas/fiap-datalake/main/csv-data/" AS base
+WITH base + "blhs-estados.csv" AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(estado:UFs {name: row.uf})
+MATCH(doadora:BLHs {id: row.id})
+MERGE (doadora)-[:RESIDE]->(estado);
